e67d367db94a61e2e794d98a83e1be3e8be13388
ahmed a <ahmed.yosri.a@gmail.com>
2018-10-17 18:41:35 -0700

    finsh Miwok

--- app/src/main/java/com/example/android/miwok/ColorsActivity.java
+++ app/src/main/java/com/example/android/miwok/ColorsActivity.java
@@ -4,7 +4,6 @@ package com.example.android.miwok;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ListView;
-
 import java.util.ArrayList;
 
 public class ColorsActivity extends AppCompatActivity {
