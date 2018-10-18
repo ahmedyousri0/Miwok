6f36a33d2724cfe347bbc9e4e8e86b4a3d51a6db
ahmed a <ahmed.yosri.a@gmail.com>
2018-10-17 18:06:45 -0700

    Remaining Words 2-30

--- app/src/main/java/com/example/android/miwok/NumbersActivity.java
+++ app/src/main/java/com/example/android/miwok/NumbersActivity.java
@@ -11,9 +11,8 @@ public class NumbersActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_numbers);
+        setContentView(R.layout.word_list);
 
-        // Create a list of words
         ArrayList<Word> words = new ArrayList<Word>();
         words.add(new Word("one", "lutti"));
         words.add(new Word("two", "otiiko"));
@@ -26,17 +25,12 @@ public class NumbersActivity extends AppCompatActivity {
         words.add(new Word("nine", "wo’e"));
         words.add(new Word("ten", "na’aacha"));
 
-        // Create an {@link WordAdapter}, whose data source is a list of {@link Word}s. The
-        // adapter knows how to create list items for each item in the list.
         WordAdapter adapter = new WordAdapter(this, words);
 
-        // Find the {@link ListView} object in the view hierarchy of the {@link Activity}.
-        // There should be a {@link ListView} with the view ID called list, which is declared in the
-        // activity_numbers.xml layout file.
+
         ListView listView = (ListView) findViewById(R.id.list);
 
-        // Make the {@link ListView} use the {@link WordAdapter} we created above, so that the
-        // {@link ListView} will display list items for each {@link Word} in the list.
+
         listView.setAdapter(adapter);
     }
 }
