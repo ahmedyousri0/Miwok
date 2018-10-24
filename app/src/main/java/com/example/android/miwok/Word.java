7762d43c291fd6373475b79bbf9be7afa168cb00
ahmed a <ahmed.yosri.a@gmail.com>
2018-10-24 08:25:52 -0700

    final prohect

--- app/src/main/java/com/example/android/miwok/Word.java
+++ app/src/main/java/com/example/android/miwok/Word.java
@@ -1,38 +1,63 @@
 package com.example.android.miwok;
 
+/**
+ * {@link Word} represents a vocabulary word that the user wants to learn.
+ * It contains a default translation, a Miwok translation, and an image for that word.
+ */
 public class Word {
 
+    /** Default translation for the word */
     private String mDefaultTranslation;
 
+    /** Miwok translation for the word */
     private String mMiwokTranslation;
+    /** Audio resource ID for the word */
+    private int mAudioResourceId;
+    /** Image resource ID for the word */
     private int mImageResourceId = NO_IMAGE_PROVIDED;
+    /** Constant value that represents no image was provided for this word */
     private static final int NO_IMAGE_PROVIDED = -1;
 
-
-    public Word(String defaultTranslation, String miwokTranslation) {
+    public Word(String defaultTranslation, String miwokTranslation, int audioResourceId) {
         mDefaultTranslation = defaultTranslation;
         mMiwokTranslation = miwokTranslation;
+        mAudioResourceId = audioResourceId;
     }
-
-    public Word(String defaultTranslation, String miwokTranslation, int imageResourceId) {
+    public Word(String defaultTranslation, String miwokTranslation, int imageResourceId,
+                int audioResourceId) {
         mDefaultTranslation = defaultTranslation;
         mMiwokTranslation = miwokTranslation;
         mImageResourceId = imageResourceId;
+        mAudioResourceId = audioResourceId;
     }
-
     public String getDefaultTranslation() {
         return mDefaultTranslation;
     }
 
+    /**
+     * Get the Miwok translation of the word.
+     */
     public String getMiwokTranslation() {
         return mMiwokTranslation;
     }
 
+    /**
+     * Return the image resource ID of the word.
+     */
     public int getImageResourceId() {
         return mImageResourceId;
     }
 
+    /**
+     * Returns whether or not there is an image for this word.
+     */
     public boolean hasImage() {
         return mImageResourceId != NO_IMAGE_PROVIDED;
+    }
+    /**
+     * Return the audio resource ID of the word.
+     */
+    public int getAudioResourceId() {
+        return mAudioResourceId;
     }
 }
